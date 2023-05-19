a809126e36848b9db78f7f16c7c249fd24d60c75
Victor Carpes <victorcarpes@outlook.com>
2023-05-19 16:24:36 +0100

    adicionando loading botão finalizar compra

--- src/components/TotalCard.tsx
+++ src/components/TotalCard.tsx
@@ -2,22 +2,26 @@ import axios from "axios";
 import { useCart } from "../hooks/useCart";
 import { TotalContainer } from "../styles/components/totalCart";
 import { formatPrice } from "../utils/formatPrice";
+import { useState } from "react";
 
 export function TotalCart() {
   const { total, cart, totalItens } = useCart();
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleBuyProduct() {
     try {
+      setIsLoading(true);
       const response = await axios.post('/api/checkout', {
         products: cart,
-      })
+      });
 
       const { checkoutUrl } = response.data;
 
       window.location.href = checkoutUrl;
     } catch (err) {
-      alert('Erro ao encaminhar para checkout, tente novamente mais tarde')
-      console.log(err)
+      alert('Erro ao encaminhar para checkout, tente novamente mais tarde');
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -30,7 +34,7 @@ export function TotalCart() {
         <p className="total">Valor total</p>
         <span className="total-value">{formatPrice(total)}</span>
       </div>
-      <button disabled={cart.length <= 0} onClick={() => handleBuyProduct()} >Finalizar compra</button>
+      <button disabled={cart.length <= 0} onClick={() => handleBuyProduct()}>{isLoading === true ? "Carregando..." : "Finalizar compra"}</button>
     </TotalContainer>
   )
 }
